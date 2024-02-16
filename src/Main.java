b8fbcb7ceb70956b1bb16b04c60dbdec6c4bf09e
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-02-16 23:21:01 +0300

    Готово

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,24 @@ public class Main {
         System.out.println(o.getClass().getSimpleName() + ": " + o);    // Double: 0.75
 
         Pair<Integer,String> pair = new Pair<>(1,"First");
-        System.out.println(pair.getFirst());
-        System.out.println(pair.getSecond());
-        System.out.println(pair.toString());
+        System.out.println(pair.getFirst());                            // 1
+        System.out.println(pair.getSecond());                           // First
+        System.out.println(pair);                                       // Pair{1,First}
 
+        Number[] arr1 = new Number[] {1,2L,1.5f};
+        Object[] arr2 = new Object[] {2,3L,4.5f};
+        Number[] arr3 = new Number[] {1,2L,1.5};
+        System.out.println(compareArrays(arr1, arr2));                  // true
+        System.out.println(compareArrays(arr1, arr3));                  // false
+    }
+
+    private static <T1, T2> boolean compareArrays(T1[] arr1, T2[] arr2) {
+        if (arr1.length != arr2.length) return false;
+        for (int i = 0; i < arr1.length; i++) {
+            if (!arr1[i].getClass().getName().equals(arr2[i].getClass().getName())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
